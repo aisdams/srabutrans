fecc99487e6f151fb48ef79a2633fea1b5011c0b
xlwzzzy <marioaistwice@gmail.com>
2024-02-05 17:53:41 +0700

    change header with image

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import HeaderImage from '../../../public/image/Srabu/header1.jpg';
+import HeaderImage from '../../../public/image/Srabu/header-1.png';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -21,15 +21,15 @@ export default function Header() {
 
   return (
     <div
-      className="relative bg-fixed bg-cover lg:bg-center bg-right-top bg-no-repeat bg-headernya z-0 min-w-full h-[100vh] overflow-hidden "
-      style={{
-        transform: `translateY(${parallaxOffset}px)`,
-        backgroundImage: `url('/image/Srabu/header1.jpg')`,
-      }}
+      className="relative bg-fixed bg-cover lg:bg-center bg-right-top bg-no-repeat bg-headernya bg- z-0 min-w-full h-[100vh] overflow-hidden w-full bg-greenOne"
+      style={{ transform: `translateY(${parallaxOffset}px)` }}
     >
-      <div className="absolute top-1/3 text-left text-white z-10 w-full lg:pl-24 pl-8">
-        <h1 className="font-bold lg:text-7xl text-6xl mb-8">SRABU TRANS</h1>
-        <h3 className="font-bold tracking-wide">
+      <div className="z-0 md:min-w-full h-[min(100vh,38rem)] overflow-hidden">
+        <Image src={HeaderImage} alt="" className="absolute right-0 lg:w-[53rem] lg:block hidden" id="home" />
+      </div>
+      <div className="bg-greenTwo/10 w-full h-[min(100vh,25.5rem)] z-10 absolute top-0 lg:pl-24 text-white my-40 lg:text-left text-center">
+        <h1 className="font-medium text-7xl mb-8">SRABU TRANS</h1>
+        <h3 className="lg:text-justify text-center font-medium tracking-wide">
           TRANSPORTATION, LOGISTIC, WAREHOUSE SERVICES, <br /> AND COMPANY OPERATIONS MANAGEMENT SYSTEM
         </h3>
         <button className="uppercase px-8 bg-greenOne border border-white hover:bg-greenTwo mt-10 rounded-full text-xs py-2 cursor-pointer">
